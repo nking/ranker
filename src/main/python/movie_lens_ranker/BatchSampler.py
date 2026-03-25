353e330309ad4dafb50ba6608d9f87ef9ae55ca4
nichole <nichole@climbwithyourfeet.com>
2026-03-25 13:19:13 -0700

    fixing a potential divide by zero error

--- src/main/python/movie_lens_ranker/BatchSampler.py
+++ src/main/python/movie_lens_ranker/BatchSampler.py
@@ -4,12 +4,13 @@ import grain
 import numpy as np
 
 class BatchSampler(grain.samplers.IndexSampler):
-    def __init__(self, num_records: int, num_epochs:int, batch_size: int, shuffle:bool=False, seed:int=0,
+    def __init__(self, num_records: int, num_epochs:int, batch_size: int,
+            shuffle:bool=False, seed:int=0,
             shard_options: grain.sharding.ShardOptions = grain.sharding.NoSharding()):
         super().__init__(num_records=num_records, shard_options=shard_options,
             shuffle=shuffle, seed=seed, num_epochs=num_epochs)
         self.batch_size = batch_size
-        self.num_batches = self._num_records // batch_size
+        self.num_batches = max(1, self._num_records // batch_size)
         
     # override
     def __repr__(self) -> str:
